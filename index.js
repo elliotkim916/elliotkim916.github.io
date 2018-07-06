2cda2128b4e2ed5ebd5a783cbfaeb36f41a77656
Elliot Kim <elliotkim916@gmail.com>
2018-07-06 16:13:07 -0700

    using querySelector for smooth scroll

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ $(document).ready(function(){
   $('a[href^="#"]').on('click', function(e) {
     e.preventDefault();
     let target = $(this).attr('href');
+    document.querySelector(target).scrollIntoView({behavior: 'smooth', inline: 'nearest'});
     
-    $(target).velocity('scroll', {
-      duration: 800,
-      offset: -40,
-      easing: 'ease-in-out'
-    }).focus();
+    // $(target).velocity('scroll', {
+    //   duration: 800,
+    //   offset: -40,
+    //   easing: 'ease-in-out'
+    // }).focus();
   });
 });

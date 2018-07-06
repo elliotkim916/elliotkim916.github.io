dcbd7463435e9accc73f2b1626bb20b89b841a1a
Elliot Kim <elliotkim916@gmail.com>
2018-07-06 16:37:19 -0700

    added block start to querySelector

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   $('a[href^="#"]').on('click', function(e) {
     e.preventDefault();
     let target = $(this).attr('href');
-    document.querySelector(target).scrollIntoView({behavior: 'smooth', inline: 'nearest'});
+    document.querySelector(target).scrollIntoView({behavior: 'smooth', block: 'start'});
     
     // $(target).velocity('scroll', {
     //   duration: 800,

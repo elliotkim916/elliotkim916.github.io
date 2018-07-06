6438fd1e0e2e97ef68ea6e923f2a48a24f114f63
Elliot Kim <elliotkim916@gmail.com>
2018-07-05 18:04:30 -0700

    able to scroll to parts of page with ease

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
 'use strict';
 
-window.jQuery = window.$ = require('path/to/jquery-x.x.x.js');
-require('path/to/velocity.js');
-// Optional: If you're using the UI pack, require it after Velocity. (You don't need to assign it to a variable.)
-require('path/to/velocity.ui.js');
-/* Your app code here. */
-$('body').velocity({ opacity: 0.5 });
-
-// function smoothScroll() {
-//   let element = document.getElemenyById('about-me');
-//   element.scrollIntoView({
-//     behavior: 'smooth'
-//   });
-// }
-
 $(document).ready(function(){
   $('a[href^="#"]').on('click', function(e){
     e.preventDefault();

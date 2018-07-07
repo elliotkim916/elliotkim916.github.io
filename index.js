b955feba834baf77088bb560507961206cf84c7f
Elliot Kim <elliotkim916@gmail.com>
2018-07-07 10:36:25 -0700

    cleaned up code, now using animate and offset to smooth scroll

--- index.js
+++ index.js
@@ -3,16 +3,9 @@
 $(document).ready(function(){
   $('a[href^="#"]').on('click', function(e) {
     e.preventDefault();
-    // let target = $(this).attr('href');
-    // document.body.querySelector(target).scrollIntoView({behavior: 'smooth', block: 'start'});
     
     $('body, html').animate({
       scrollTop: $(this.hash).offset().top
-    }, 1000 );
-    // $(target).velocity('scroll', {
-    //   duration: 800,
-    //   offset: -40,
-    //   easing: 'ease-in-out'
-    // }).focus();
+    }, 1000).focus();
   });
 });

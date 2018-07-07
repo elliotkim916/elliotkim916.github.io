20243c2dd3668ec7f35877b5f2403e9cda471e11
Elliot Kim <elliotkim916@gmail.com>
2018-07-06 18:42:26 -0700

    using animate for smooth scrolling

--- index.js
+++ index.js
@@ -3,9 +3,12 @@
 $(document).ready(function(){
   $('a[href^="#"]').on('click', function(e) {
     e.preventDefault();
-    let target = $(this).attr('href');
-    document.querySelector(target).scrollIntoView({behavior: 'smooth', block: 'start'});
+    // let target = $(this).attr('href');
+    // document.body.querySelector(target).scrollIntoView({behavior: 'smooth', block: 'start'});
     
+    $('body, html').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1000 );
     // $(target).velocity('scroll', {
     //   duration: 800,
     //   offset: -40,
